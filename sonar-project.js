1b352f9b41edd17623a3f8efe6317601a4ad58ec
barathKawin <80077511+barathKawin@users.noreply.github.com>
2021-04-22 13:51:00 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://52.66.4.70:9000/',
+  serverUrl: 'http://65.1.147.224:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
